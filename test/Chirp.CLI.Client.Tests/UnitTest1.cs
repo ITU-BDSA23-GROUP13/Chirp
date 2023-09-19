a355126035a3245b864869ea7b5011917cedcf10
Connie Petersson <143688350+copeitu@users.noreply.github.com>
2023-09-19 15:17:12 +0200

    Unitest and solution file added

---
+++ test/Chirp.CLI.Client.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Chirp.CLI.Client.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var input = 99;
+        // Act
+        var result = true;
+        // Assert
+        Assert.False(result);
+    }
+}
