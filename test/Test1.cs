a35b655a37284f8891a50d3382096d672eb60da0
Connie Petersson <143688350+copeitu@users.noreply.github.com>
2023-09-19 14:21:29 +0200

    Add repository for test

---
+++ test/Test1.cs
@@ -0,0 +1,10 @@
+[Fact]
+public void NinetynineIsNotPrime()
+{
+    // Arrange
+    var input = 99;
+    // Act
+    var result = _primeService.IsPrime(input);
+    // Assert
+    Assert.False(result);
+}
