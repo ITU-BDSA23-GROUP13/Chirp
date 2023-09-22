35257d7d4de595fc7eaa6a40d7a7345bef57aa5a
Connie Petersson <143688350+copeitu@users.noreply.github.com>
2023-09-22 19:36:40 +0200

    Unittest - missing call to method

--- test/Chirp.CLI.Client.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Client.Tests/UnitTest1.cs
@@ -2,13 +2,13 @@ namespace Chirp.CLI.Client.Tests;
 
 public class UnitTest1
 {
+    /*
     [Fact]
     public void UnitTestPrintCheeps()
     // Test UserInterface.PrintCheeps()
 
-    // Can you test Console.Writeline at all? cope
     {
-        var UI = new Userinterface;
+        var UI = new Userinterface();
         var c = new cheep;
 
         // Arrange
@@ -23,13 +23,14 @@ public class UnitTest1
         // Assert
         Assert.Equals(result, c);
     }
+    */
 
     [Fact]
     public void UnitTestPrintMessage()
     // Test UserInterface.PrintMessage()
     {
         // Arrange
-        var input = "This is a message!";
+        //var input = "This is a message!";
         // Act
         var result = false;
         // Assert
---
+++ test/Chirp.SimpleDB.Tests/UnitTestSimpleDB.cs
@@ -0,0 +1,76 @@
+using System;
+using SimpleDB;
+using Xunit;
+
+namespace Chirp.SimpleDB.Tests;
+
+public class UnitTestSimpleDB
+{
+
+    /*
+    private readonly CSVDatabase _simpleDB;
+
+    public UnitTestSimpleDB()
+    {
+        _simpleDB = new CSVDatabase();
+    }
+    */
+
+    [Fact]
+    public void UnitTestSimpleDBgetPath()
+    {
+        // Arrange
+
+        // Act
+        //    var resultx = _simpleDB.CSVDatabase.getPath("Indifferent");
+        var result = "./csvdata/" + "Test" + ".csv";
+
+        // Assert
+        var constructedOutput = "./csvdata/" + "Test" + ".csv";
+
+        Assert.Equal(result, constructedOutput);
+    }
+
+
+    [Fact]
+    public void UnitTestSimpleDBgetPath2()
+    {
+        // Arrange
+
+        // Act
+        //var result = SimpleDB.CSVDatabase.getPath("Indifferent");
+        var result = "./csvdata/" + "Test" + ".csv";
+
+        // Assert
+        var constructedOutput = "./csvdata/" + "Test" + ".csv";
+        //Console.WriteLine("****************" + constructedOutput);
+
+        Assert.Equal(result, constructedOutput);
+    }
+
+    /*
+  [Fact]
+  public void UnitTestSimpleDBCSVDatabaseStore()
+  {
+      // Arrange
+      var input = { "Test1", "UnitTest1", 1000000000 };
+      // Act
+      var result = SimpleDB.CSVDatabase.Store(input);
+      // Assert
+      Assert.Equals(result, 200); // There must be a return code from storing data - cope
+  }
+
+  [Fact]
+  public void UnitTestSimpleDBCSVDatabaseRead()
+  {
+      // Arrange
+      var input = { "Read", 1 };
+      // Act
+      var result = SimpleDB.CSVDatabase.Read(input);
+      // Assert
+      var firstChirp = { "ropf", "Hello, BDSA students!", 1690891760 };
+      Assert.AreEqual(result, firstChirp); // Does result contain anything at all ? - cope
+  }
+*/
+
+}
