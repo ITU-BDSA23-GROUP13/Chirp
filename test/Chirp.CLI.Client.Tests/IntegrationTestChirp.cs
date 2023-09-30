93e9e36d9cabc9b5f355684c33a72dabb5933849
Connie Petersson <143688350+copeitu@users.noreply.github.com>
2023-09-30 12:22:33 +0200

    Integration test updated - running call to UserInterface method PrintMesage Co-author Christoffer

--- test/Chirp.CLI.Client.Tests/IntegrationTestChirp.cs
+++ test/Chirp.CLI.Client.Tests/IntegrationTestChirp.cs
@@ -3,23 +3,54 @@ using Xunit;
 using Xunit.Abstractions;
 using Microsoft.VisualStudio.TestPlatform.Utilities;
 using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
 namespace Chirp.CLI.Client.Tests
 {
-    public class IntegrationTestChirp
+    public class IntegrationTestChirpUserinterface
     {
         public record Cheep(string Author, string Message, long Timestamp);
 
         [Fact]
-        public void ToBeDeleted_NoTest()
+        public void UserInterfacePrintMessageTest1()
         {
             // Arrange
+            bool wasExecuted = false;
 
             // Act
-            var test = true;
+            UserInterface.PrintMessage("******");
+            wasExecuted = true;
+
             // Assert
-            Assert.True(test);
+            Assert.True(wasExecuted);
 
         }
+
+        [Fact]
+        public void UserInterfacePrintMessageTest2()
+        {
+            // Arrange
+            bool wasExecuted = false;
+            // var expectedOutput = "User1 @ 09/26/21 00:00:00 Output er for kort!\r\n";
+            var expectedOutput = "Output er for kort!\r\n";
+
+            // Act
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                UserInterface.PrintMessage("Output er for kort!");
+                wasExecuted = true;
+
+                var x = sw.ToString();
+
+                // Assert
+                Assert.True(wasExecuted);
+                Assert.Equal(expectedOutput, sw.ToString());
+            }
+        }
+
     }
+
 }
