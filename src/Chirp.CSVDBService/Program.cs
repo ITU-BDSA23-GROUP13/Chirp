c5d83f184aacac1321acd2a18916696d283a68ee
marc9622 <maaw@itu.dk>
2023-10-01 19:43:14 +0200

    First attempt at WebAPI

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -1,8 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using SimpleDB;
 
-//app.MapGet("/", () => "Hello World!");
-app.MapGet("/cheeps", () => new Cheep("me", "Hej!", 1684229348));
+namespace Chirp.CsvDbService;
 
-app.Run();
-public record Cheep(string Author, string Message, long Timestamp);
+public class Program
+{
+    public record Cheep(string Author, string Message, long Timestamp);
+
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        IDatabase<Cheep> db = CsvDatabase<Cheep>.Instance("cheeps");
+
+        app.MapGet("/", () => "Hello World!");
+        app.MapGet("/cheeps", () => db.Read().Select(cheep => cheep.ToString()).Aggregate((a, b) => $"{a}\n{b}"));
+        app.MapPost("/cheep", (Cheep cheep) => db.Store(cheep));
+
+        app.Run();
+    }
+}
