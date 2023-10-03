0979b2b67f787101087981c2d57f72fc3f5f522e
marc9622 <maaw@itu.dk>
2023-10-03 16:34:28 +0200

    Changing CLI to retrieve data from web API

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using SimpleDB;
 using CommandLine;
+using System.Net;
+using System.Net.Http.Json;
 
 namespace Chirp.CLI;
 
@@ -7,17 +9,50 @@ public class Program
 {
     public record Cheep(string Author, string Message, long Timestamp);
 
+    private const string serverAddress = "https://bdsagroup13chirpremotedb.azurewebsites.net";
+
     private static void Main(string[] args)
     {
         Parser.Default.ParseArguments<ChirpArguments.Options>(args)
-        .WithParsed(options =>
+        .WithParsed(async (ChirpArguments.Options options) =>
         {
             IDatabase<Cheep> db = CsvDatabase<Cheep>.Instance("cheeps");
 
             //Read cheeps
             if (options.CheepCount != null)
             {
-                var cheeps = db.Read(options.CheepCount.Value);
+                //var cheeps = db.Read(options.CheepCount.Value);
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(serverAddress);
+
+                HttpResponseMessage response;
+                try {
+                    response = await client.GetAsync("/");
+                }
+                catch (Exception e)
+                {
+                    UserInterface.PrintMessage(e.Message);
+                    return;
+                }
+
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    UserInterface.PrintMessage("Failed to connect to server.");
+                    return;
+                }
+
+                var cheeps = await System.Net.Http.Json.HttpContentJsonExtensions.ReadFromJsonAsync<List<Cheep>>(response.Content);
+                if (cheeps is null)
+                {
+                    UserInterface.PrintMessage("Failed to read cheeps from server.");
+                    return;
+                }
+                if (cheeps.Count == 0)
+                {
+                    UserInterface.PrintMessage("No cheeps found.");
+                    return;
+                }
+
                 UserInterface.PrintCheeps(cheeps);
             }
 
