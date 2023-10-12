434ef79fff689015f9580977a537b626f5f1f645
Daniel Ahmadi <daah@itu.dk>
2023-10-12 14:34:14 +0200

    working on sqlite

--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -16,21 +16,18 @@ public class DBFacade
             connection.Open();
 
             var command = connection.CreateCommand();
-            command.CommandText =
-            @"
-            SELECT username
-            FROM user
-            WHERE user_id = 5
-            ";
+            
 
-            using var reader = command.ExecuteReader();
-            while(reader.Read())
-            {
-                Object[] values = new Object[reader.FieldCount];
-                reader.GetValues(values);
-
-                Console.WriteLine(values[0]);
-            }
+            using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        // Assuming there's only one result row
+                        string username = reader.GetString(0);
+                        Console.WriteLine($"Username: {username}");
+                    }
+                }
+            
 
             connection.Close();
         }
