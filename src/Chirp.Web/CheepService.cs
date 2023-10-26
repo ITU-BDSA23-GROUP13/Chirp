78961f1cd50f55ae5ec92d030e49754e9cac3996
Daniel Ahmadi <daah@itu.dk>
2023-10-26 18:18:35 +0200

    Now shops 32 cheeps per page. #81

--- src/Chirp.Web/CheepService.cs
+++ src/Chirp.Web/CheepService.cs
@@ -7,18 +7,24 @@ public record CheepViewModel(string Author, string Message, DateTimeOffset Times
 
 public interface ICheepService
 {
-    public Task<List<CheepViewModel>> GetCheeps();
+    public Task<List<CheepViewModel>> GetCheeps(int page);
     public Task<List<CheepViewModel>> GetCheepsFromAuthor(string author);
 }
 
 public class CheepService : ICheepService
 {
+    private int pageSize = 32;
     private IChirpRepository chirpRepository = new ChirpRepository();
 
-    public async Task<List<CheepViewModel>> GetCheeps()
+    public async Task<List<CheepViewModel>> GetCheeps(int page)
     {
         var cheeps = await chirpRepository.ReadCheeps();
-        return cheeps.Select(c => new CheepViewModel(c.Author, c.Text, DateTimeOffset.FromUnixTimeSeconds(c.Timestamp))).ToList();
+        return cheeps
+            .OrderByDescending(cheep => cheep.Timestamp)
+            .Skip((page-1) * pageSize)
+            .Take(pageSize)
+            .Select(c => new CheepViewModel(c.Author, c.Text, DateTimeOffset.FromUnixTimeSeconds(c.Timestamp)))
+            .ToList();
     }
 
     public async Task<List<CheepViewModel>> GetCheepsFromAuthor(string author)
