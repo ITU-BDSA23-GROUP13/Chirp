d87ac6923a996b2cbd57855d0180f913d8606fb5
Connie Petersson <143688350+copeitu@users.noreply.github.com>
2023-10-29 08:33:48 +0100

    Test is running again

---
+++ test/Web.Tests/UnitTestWeb.cs
@@ -0,0 +1,82 @@
+namespace Web.Tests;
+
+public class UnitTestWeb
+
+// Infrastructure
+
+{
+    /************************
+        Dummy test
+    *************************/
+    [Fact]
+    public void NinetynineIsNotPrime() // Dummy test
+    {
+        // Arrange
+        var input = 99;
+        // Act
+        var result = false;
+        // Assert
+        Assert.False(result);
+    }
+
+    /************************
+        Test DBFacade.cs
+    *************************/
+    [Fact]
+    public void OKTestDBFacadeRunDB() // Dummy test
+    {
+        // DB is running
+
+        // Arrange
+        var input = 99;
+        // Act
+        var result = false;
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void NotOKTestDBFacadeRunDB() // Dummy test
+    {
+        // Failure running DB
+
+        // Arrange
+        var input = 99;
+        // Act
+        var result = false;
+        // Assert
+        Assert.False(result);
+    }
+
+
+    /************************
+        Test CheeoService.cs
+    *************************/
+    [Fact]
+    public void OKTestDBCheepServiceGetCheeps() // Dummy test
+    {
+        // Cheeps are extracted
+
+        // Arrange
+        var input = 99;
+        // Act
+        var result = false;
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void NotOKTestDBCheepServiceGetCheeps() // Dummy test
+    {
+        // Cheeps are not extracted
+        // The wrong Cheeps are extracted
+
+        // Arrange
+        var input = 99;
+        // Act
+        var result = false;
+        // Assert
+        Assert.False(result);
+    }
+
+}
