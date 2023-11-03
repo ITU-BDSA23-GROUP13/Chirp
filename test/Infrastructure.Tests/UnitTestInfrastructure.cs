f0870b95fec6848051c4d525eb5a58a40a15e5d0
Connie Petersson <143688350+copeitu@users.noreply.github.com>
2023-11-03 18:32:27 +0100

    Test20231103_1 - 5

--- test/Infrastructure.Tests/UnitTestInfrastructure.cs
+++ test/Infrastructure.Tests/UnitTestInfrastructure.cs
@@ -1,23 +1,111 @@
-namespace Infrastructure.Tests;
+// namespace Infrastructure.Tests; 
+// this needs to be moved !!!!
 
-public class UnitTests
+/*********************************************
+    UnitTest Chirp Repository
+**********************************************
+Tests units:
+    GetAuthorIdFromName(String author)
+    ReadCheeps()
+    ChirpRepository()
+    ReadCheepsFromAuthor(ulong authorId)
 
-// Infrastructure
+Not implemented yet:
+    CreateCheep(Guid id, CheepDTO cheepDTO)
+    CreateAuthor(Guid id, AuthorDTO authorDTO)
+    ReadAuthors()
+    ReadCheep(Guid id)
+    ReadAuthor(ulong id)
+    UpdateAuthor(AuthorDTO author)
+    UpdateCheep(CheepDTO cheep)
+    DeleteCheep(Guid id)
+    DeleteAuthor(Guid id)    
+
+***********************************************/
+using Chirp.Infrastructure;
+//namespace Chirp;
+using Chirp;
+
+public class UnitTestsInfrastructure
 
 {
-    /************************
-        Dummy test
-    *************************/
     [Fact]
-    public void NinetynineIsNotPrime() // Dummy test
+    public async Task UnitTEstGetAuthorIdFromName()
     {
         // Arrange
-        var input = 99;
+        var input = "Mellie Yost";
+        //var input = "ropf";
+        var x = new ChirpRepository();
+
         // Act
+        console.Writeline("**************");
+        // GetAuthorIdFromName returns a <ulong>-value
+        var author = await x.GetAuthorIdFromName(input);
+        console.Writeline(author.ToString());
+
         var result = false;
+        if (author > 0)
+        {
+            result = true;
+        }
+
         // Assert
-        Assert.False(result);
+        Assert.false(result == true);
     }
 
+    /*
+        [Fact]
+        public async Task NotUnitTEstGetAuthorIdFromName() // Dummy test
+        {
+            // Arrange
+            var input = "XXXX";
+            // Act
+            var author = GetAuthorIdFromName(input);
 
+            var result;
+            if (author == null)
+            {
+                result = false;
+            }
+            else
+            {
+                result = true;
+            }
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task UnitTestReadCheeps() // Dummy test
+        {
+            // Arrange
+            var input = 99;
+            // Act
+            var result = ReadCheeps();
+            // Assert
+            Assert.False(null(result));
+        }
+
+        [Fact]
+        public async Task UnitTestChirpRepository() // Dummy test
+        {
+            // Arrange
+
+            // Act
+            var result = ChirpRepository();
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async Task UnitTestReadCheepsFromAuthor() // Dummy test
+        {
+            // Arrange
+            var input = 0;
+            // Act
+            var result = ReadCheepsFromAuthor(input);
+            // Assert
+            Assert.False(result);
+        }
+    */
 }
