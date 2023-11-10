ab48b31056747c5a289daf365dd20f43c1c83d94
Connie Petersson <143688350+copeitu@users.noreply.github.com>
2023-11-10 22:07:23 +0100

    Test commit

--- test/End2EndTests.cs
+++ test/End2EndTests.cs
@@ -14,33 +14,33 @@ public class End2End
         var result = IsPrime(input);
         // Assert
         Assert.False(result);
-
-        /*
-            [Fact]
-            public void TestReadCheep()
+    } // New line
+    /*
+        [Fact]
+        public void TestReadCheep()
+        {
+            // Arrange
+            ArrangeTestDatabase();
+            // Act
+            string output = "";
+            using (var process = new Process())
             {
-                // Arrange
-                ArrangeTestDatabase();
-                // Act
-                string output = "";
-                using (var process = new Process())
-                {
-                    process.StartInfo.FileName = "/usr/bin/dotnet";
-                    process.StartInfo.Arguments = "./src/Chirp.CLI.Client/bin/Debug/net7.0/chirp.dll read 10";
-                    process.StartInfo.UseShellExecute = false;
-                    process.StartInfo.WorkingDirectory = "../../../../../";
-                    process.StartInfo.RedirectStandardOutput = true;
-                    process.Start();
-                    // Synchronously read the standard output of the spawned process.
-                    StreamReader reader = process.StandardOutput;
-                    output = reader.ReadToEnd();
-                    process.WaitForExit();
-                }
-                string fstCheep = output.Split("\n")[0];
-                // Assert
-                Assert.StartsWith("ropf", fstCheep);
-                Assert.EndsWith("Hello, World!", fstCheep);
+                process.StartInfo.FileName = "/usr/bin/dotnet";
+                process.StartInfo.Arguments = "./src/Chirp.CLI.Client/bin/Debug/net7.0/chirp.dll read 10";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.WorkingDirectory = "../../../../../";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.Start();
+                // Synchronously read the standard output of the spawned process.
+                StreamReader reader = process.StandardOutput;
+                output = reader.ReadToEnd();
+                process.WaitForExit();
             }
-            */
+            string fstCheep = output.Split("\n")[0];
+            // Assert
+            Assert.StartsWith("ropf", fstCheep);
+            Assert.EndsWith("Hello, World!", fstCheep);
+        }
+        */
 
-    }
+}
--- test/Infrastructure.Tests/UnitTestInfrastructure.cs
+++ test/Infrastructure.Tests/UnitTestInfrastructure.cs
@@ -1,123 +1,42 @@
-using Chirp.Infrastructure;
-
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
-
 /*********************************************
     UnitTest Chirp Repository
-**********************************************
-Tests units:
-    GetAuthorIdFromName(String author)
-    ReadCheeps()
-    ChirpRepository()
-    ReadCheepsFromAuthor(ulong authorId)
+**********************************************/
 
-Not implemented yet:
-    CreateCheep(Guid id, CheepDTO cheepDTO)
-    CreateAuthor(Guid id, AuthorDTO authorDTO)
-    ReadAuthors()
-    ReadCheep(Guid id)
-    ReadAuthor(ulong id)
-    UpdateAuthor(AuthorDTO author)
-    UpdateCheep(CheepDTO cheep)
-    DeleteCheep(Guid id)
-    DeleteAuthor(Guid id)    
+using Chirp.Infrastructure;
 
-***********************************************/
 namespace Chirp.Tests;
 
+using static Chirp.Core.ICheepRepository;
+
 public class UnitTestsInfrastructure
-
 {
-    /*
+
     [Fact]
-    public void UnitTestReadNumberOfCheeps()
-    // Remove if too much disturbance
+    public void IntegrationTestDummyInfrastructure() // Dummy test
     {
         // Arrange
-        var x = new ChirpRepository();
-        var CountCheeps = 99;
+        //var input = 99;
         // Act
-        var result = ReadNumberOfCheeps();
+        var result = false;
         // Assert
-        Assert.False(result == CountCheeps);
-
+        Assert.False(result);
     }
 
-    [Fact]
-    public int UnitTestReadNumberOfPagesOfCheeps()
-    // Remove if too much disturbance
-    {
-        // Arrange
-        var x = new ChirpRepository();
-        var CountPages = 9;
-        // Act
-        var result = ReadNumberOfPagesOfCheeps();
-        // Assert
-        Assert.False(result == CountPages);
-    }
-    */
 
-    [Fact]
-    public async Task UnitTestGetAuthorIdFromName()
-    {
-        // Arrange
-        var input = "Mellie Yost";
-        //var input = "ropf";
-        //var input = "Helge";
-        var cR = new ChirpRepository();
+    // [Fact]
+    // public async Task NotUnitTestGetAuthor() // Dummy test
+    // {
+    //     // Arrange
+    //     var input = 1000000;
+    //     var cR = new CheepRepository.ChirpContext();
 
-        // Act
-        // GetAuthorIdFromName returns a <ulong>-value
-        var _ = await cR.GetAuthorIdFromName(input);
-    }
+    //     // Act
+    //     Func<Task> action = async () => await cR.GetAuthor(input);
 
-    [Fact]
-    public async Task NotUnitTEstGetAuthorIdFromName() // Dummy test
-    {
-        // Arrange
-        var input = "XXXX";
-        var cR = new ChirpRepository();
-        // Act
-        // var msg = await cR.GetAuthorIdFromName(input);
-        Func<Task> action = async () => await cR.GetAuthorIdFromName(input);
+    //     // Assert
+    //     var ex = await Assert.ThrowsAsync<InvalidOperationException>(action);
+    //     Assert.Contains("Sequence contains no elements.", ex.Message);
+    // }
 
-        // Assert
-        var ex = await Assert.ThrowsAsync<InvalidOperationException>(action);
-        Assert.Contains("Sequence contains no elements.", ex.Message);
-    }
-    /*
-            [Fact]
-            public async Task UnitTestReadCheeps() // Dummy test
-            {
-                // Arrange
-                var input = 99;
-                // Act
-                var result = ReadCheeps();
-                // Assert
-                Assert.False(null(result));
-            }
-
-            [Fact]
-            public async Task UnitTestChirpRepository() // Dummy test
-            {
-                // Arrange
-
-                // Act
-                var result = ChirpRepository();
-                // Assert
-                Assert.False(result);
-            }
-
-            [Fact]
-            public async Task UnitTestReadCheepsFromAuthor() // Dummy test
-            {
-                // Arrange
-                var input = 0;
-                // Act
-                var result = ReadCheepsFromAuthor(input);
-                // Assert
-                Assert.False(result);
-            }
-        */
 }
+
