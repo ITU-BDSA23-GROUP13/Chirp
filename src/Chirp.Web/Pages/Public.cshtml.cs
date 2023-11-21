afcc38dd77e4fc241fccad3c05f3ce4013722970
marc9622 <maaw@itu.dk>
2023-11-21 16:41:45 +0100

    Added page number and page count to public model.

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -9,20 +9,31 @@ public class PublicModel : PageModel
 {
     private readonly ICheepService service;
     public required List<CheepViewModel> Cheeps { get; set; }
-    public required uint TotalCount { get; set; }
+
+    [FromQuery(Name = "page")]
+    public required uint PageNumber { get; set; }
+    public required uint TotalPageCount { get; set; }
 
     public PublicModel(ICheepService service)
     {
         this.service = service;
     }
 
-    public async Task<ActionResult> OnGetAsync([FromQuery(Name = "page")] uint page = 1)
+    public async Task<ActionResult> OnGetAsync()
     {
-        var result = await service.GetCheepsAndTotalCount(page);
+        var result = await service.GetCheepsAndPageCount(PageNumber);
 
         Cheeps = result.Item1;
-        TotalCount = result.Item2;
+        TotalPageCount = result.Item2;
 
         return Page();
+    }
+
+    [BindProperty]
+    public CheepViewModel? CheepViewModel { get; set; }
+
+    public Task<ActionResult> OnPostAsync()
+    {
+        throw new NotImplementedException();
     }
 }
