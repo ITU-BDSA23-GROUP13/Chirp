a690cafa12acc3b2d0c8b8a3101fe33d5aaca96c
marc9622 <maaw@itu.dk>
2023-11-21 14:39:18 +0100

    Fixing github client id/secret

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -43,7 +43,7 @@ builder.Services.Configure<IdentityOptions>(options =>
 builder.Services.AddAuthentication()
     .AddGitHub(options =>
     {
-        options.ClientId     = "57d22a4e96859177858c";
+        options.ClientId     = /*"57d22a4e96859177858c";*/ "Iv1.dcb4bc25e1621f2c";
         options.ClientSecret = System.Environment.GetEnvironmentVariable("CHIRP_GITHUB_CLIENT_SECRET") ?? throw new System.NullReferenceException("CHIRP_GITHUB_CLIENT_SECRET not set");
     });
     // f1e958fd2497d14e71de2156136d8e1b8a976807
