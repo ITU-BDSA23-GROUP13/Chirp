64dd200e9b94adae0a07df4c27b276911375e6ba
Daniel Ahmadi <daah@itu.dk>
2023-12-05 13:26:22 +0100

    About me page added

---
+++ src/Chirp.Web/Pages/AboutMe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages
+{
+    public class AboutMeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
